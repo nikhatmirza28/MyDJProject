d95242b28011df216497fa291c0b148354684b2a
nikhatmirza28 <nikhatkhanmirza@gmail.com>
2023-12-31 17:20:30 +0530

    firstcommit

--- MovieProject/MovieApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from MovieApp.models import Movies
-
-#Register your models here.
-class MoviesAdmin(admin.ModelAdmin):
-    list_display=['releasedate','moviename', 'actor','actress','rating'];
-
-admin.site.register(Movies,MoviesAdmin);
-
--- MovieProject/MovieApp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms;
-from MovieApp.models import Movies
-
-class MoviesForm(forms.ModelForm):
-    #no-separate fields are required(taken from model-Movies-class)
-    class Meta:
-        model=Movies
-        fields='__all__'
